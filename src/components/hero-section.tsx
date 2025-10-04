37f2d334c034d223d134bbc1af5b685a74aefdc6
Karthikeyan Natarajan <karthiknitt@gmail.com>
2025-10-05 01:16:25 +0530

    feat(ci): add environment variables for type checking and testing in CI workflow

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { HeroHeader } from './header';
 import { InfiniteSlider } from '@/components/ui/infinite-slider';
@@ -67,77 +68,77 @@ export default function HeroSection() {
               <div className="relative py-6 md:w-[calc(100%-11rem)]">
                 <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-5 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/nvidia.svg"
                       alt="Nvidia Logo"
-                      height="20"
-                      width="auto"
+                      width={120}
+                      height={20}
                     />
                   </div>
 
                   <div className="flex">
-                    <img
-                      className="mx-auto h-4 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/column.svg"
                       alt="Column Logo"
-                      height="16"
-                      width="auto"
+                      width={80}
+                      height={16}
                     />
                   </div>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-4 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/github.svg"
                       alt="GitHub Logo"
-                      height="16"
-                      width="auto"
+                      width={80}
+                      height={16}
                     />
                   </div>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-5 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/nike.svg"
                       alt="Nike Logo"
-                      height="20"
-                      width="auto"
+                      width={60}
+                      height={20}
                     />
                   </div>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-5 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
                       alt="Lemon Squeezy Logo"
-                      height="20"
-                      width="auto"
+                      width={120}
+                      height={20}
                     />
                   </div>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-4 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/laravel.svg"
                       alt="Laravel Logo"
-                      height="16"
-                      width="auto"
+                      width={100}
+                      height={16}
                     />
                   </div>
                   <div className="flex">
-                    <img
-                      className="mx-auto h-7 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/lilly.svg"
                       alt="Lilly Logo"
-                      height="28"
-                      width="auto"
+                      width={80}
+                      height={28}
                     />
                   </div>
 
                   <div className="flex">
-                    <img
-                      className="mx-auto h-6 w-fit dark:invert"
+                    <Image
+                      className="mx-auto dark:invert"
                       src="https://html.tailus.io/blocks/customers/openai.svg"
                       alt="OpenAI Logo"
-                      height="24"
-                      width="auto"
+                      width={100}
+                      height={24}
                     />
                   </div>
                 </InfiniteSlider>
